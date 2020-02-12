cbc54c02a8fc2df6fba431d71f27273ad416331b
C-Beams <c01313@gmail.com>
2020-02-11 21:54:36 -0500

    Added simple console app that demonstrates issue with GetRecords

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using CsvHelper;
+using Newtonsoft.Json;
+
+namespace CsvHelperIssueDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var csvSource = Encoding.Default.GetBytes(Properties.Resources.sample_data);
+
+            try
+            {
+                Console.WriteLine("Calling GetRecords with ToList");
+
+                var listRecords = ReturnRecordsAsList(csvSource);
+
+                Console.WriteLine("Calling ToArray() on result List");
+
+                var listRecsArray = listRecords.ToArray();
+
+                Console.WriteLine($"Result:\r\n" +
+                                  $"{JsonConvert.SerializeObject(listRecsArray, Formatting.Indented)}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            try
+            {
+                Console.WriteLine("Calling GetRecords without ToList");
+
+                var iEnumerableRecords = ReturnRecordsAsIEnumerable(csvSource);
+
+                Console.WriteLine("Calling ToArray() on result IEnumerable");
+
+                var iEnumRecsArray = iEnumerableRecords.ToArray();
+
+                Console.WriteLine($"Result:\r\n" +
+                                  $"{JsonConvert.SerializeObject(iEnumRecsArray, Formatting.Indented)}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static List<SampleModel> ReturnRecordsAsList(byte[] source)
+        {
+            var memStream = new MemoryStream(source);
+
+            using var reader = new StreamReader(memStream);
+
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                csv.Configuration.HasHeaderRecord = true;
+
+                return csv.GetRecords<SampleModel>().ToList();
+            }
+        }
+
+        public static IEnumerable<SampleModel> ReturnRecordsAsIEnumerable(byte[] source)
+        {
+            var memStream = new MemoryStream(source);
+
+            using var reader = new StreamReader(memStream);
+
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                csv.Configuration.HasHeaderRecord = true;
+
+                return csv.GetRecords<SampleModel>();
+            }
+        }
+    }
+}
---
+++ SampleModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsvHelperIssueDemo
+{
+    public class SampleModel
+    {
+        public string Value1 { get; set; }
+        public string Value2 { get; set; }
+        public string Value3 { get; set; }  
+    }
+}
